4db2f1745f58e8351e5ffdd8b94c43dceea783b6
@web-developer032 <mubasherali0331@gmail.com>
2024-06-01 01:14:29 +0500

    removed global logger and added for get all employee function

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -7,16 +7,19 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from "@nestjs/common";
 import { EmployeeService } from "./employee.service";
 import { Prisma } from "@prisma/client";
 import { USER_ROLE } from "src/users/dto/users.dto";
 import { Throttle, SkipThrottle } from "@nestjs/throttler";
+import { MyLoggerService } from "src/my-logger/my-logger.service";
 
 @SkipThrottle() // SKIP THROTTLING FOR ALL REQUESTS
 @Controller("employee")
 export class EmployeeController {
   constructor(private readonly employeeService: EmployeeService) {}
+  private readonly logger = new MyLoggerService(EmployeeController.name);
 
   @Throttle({
     short: {
@@ -35,7 +38,8 @@ export class EmployeeController {
 
   @SkipThrottle({ default: false }) // DON'T SKIP THROTTLING FOR ALL REQUESTS
   @Get()
-  findAll(@Query("role") role?: USER_ROLE) {
+  findAll(@Ip() ip: string, @Query("role") role?: USER_ROLE) {
+    this.logger.log(`GET ALL EMPLOYEES \t ${ip}`);
     return this.employeeService.findAll(role);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { MyLoggerService } from "./my-logger/my-logger.service";
+
+// import { MyLoggerService } from "./my-logger/my-logger.service";
+// ----------------------
+// TO USER GLOBAL LOGGER
+// ----------------------
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {
+//     bufferLogs: true,
+//   });
+//   app.useLogger(app.get(MyLoggerService));
+//   app.setGlobalPrefix("api");
+//   app.enableCors();
+//   await app.listen(3000);
+// }
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bufferLogs: true,
-  });
-  app.useLogger(app.get(MyLoggerService));
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.enableCors();
   await app.listen(3000);
